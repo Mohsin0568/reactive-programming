442fc36200bef63940eb9ae98c89e1481c447f27
Mohsin0568 <mohsin68.murtuza@gmail.com>
2023-01-27 11:32:52 +0530

    added simple flux and mono service

---
+++ reactive-programming-with-reactor/src/main/java/com/systa/reactive/service/FluxAndMonoGenerationService.java
@@ -0,0 +1,27 @@
+package com.systa.reactive.service;
+
+import java.util.Arrays;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+public class FluxAndMonoGenerationService {
+	
+	public Flux<String> namesFlux(){
+		return Flux.fromIterable(Arrays.asList("Alex", "Smith", "John")).log();
+	}
+	
+	public Mono<String> nameMono(){
+		return Mono.just("Alex").log();
+	}
+
+	public static void main(String[] args) {
+		
+		FluxAndMonoGenerationService service = new FluxAndMonoGenerationService();
+		service.namesFlux().subscribe(name -> System.out.println("Flux Name: " + name));
+		
+		service.nameMono().subscribe(name -> System.out.println("Mono Name: " + name));
+	}
+
+}
