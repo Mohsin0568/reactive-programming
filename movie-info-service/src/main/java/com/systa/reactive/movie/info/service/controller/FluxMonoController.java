ee9b16801b606cdc471c9c3393ca08c0fdd45568
Mohsin0568 <mohsin68.murtuza@gmail.com>
2023-01-30 22:42:36 +0530

    added new endpoint to demonstrate mono

--- movie-info-service/src/main/java/com/systa/reactive/movie/info/service/controller/FluxMonoController.java
+++ movie-info-service/src/main/java/com/systa/reactive/movie/info/service/controller/FluxMonoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * @author mohsin
@@ -20,5 +21,10 @@ public class FluxMonoController {
 	@GetMapping("/flux")
 	public Flux<Integer> getFlux(){
 		return Flux.just(1, 2, 3, 4).log();
+	}
+	
+	@GetMapping("/mono")
+	public Mono<String> getMono(){
+		return Mono.just("This is first mono").log();
 	}
 }
