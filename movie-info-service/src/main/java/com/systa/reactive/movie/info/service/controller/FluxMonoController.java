36c479669985c069a3264ed88847bab9479ad4b1
Mohsin0568 <mohsin68.murtuza@gmail.com>
2023-01-30 22:54:13 +0530

    added streams endpoint

--- movie-info-service/src/main/java/com/systa/reactive/movie/info/service/controller/FluxMonoController.java
+++ movie-info-service/src/main/java/com/systa/reactive/movie/info/service/controller/FluxMonoController.java
@@ -3,6 +3,9 @@
  */
 package com.systa.reactive.movie.info.service.controller;
 
+import java.time.Duration;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,10 @@ public class FluxMonoController {
 	@GetMapping("/mono")
 	public Mono<String> getMono(){
 		return Mono.just("This is first mono").log();
+	}
+	
+	@GetMapping(value = "/streams", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Long> stream(){
+		return Flux.interval(Duration.ofSeconds(1)).log();
 	}
 }
