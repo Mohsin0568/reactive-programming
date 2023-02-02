45cb8775fdde3237116b436201fef3836543dad3
Mohsin0568 <mohsin68.murtuza@gmail.com>
2023-02-02 22:15:16 +0530

    added few test cases like to findAll, delete, update and findById cases for movieinfo repository

--- movie-info-service/src/test/java/com/systa/reactive/movie/info/repository/MovieInfoRepositoryTest.java
+++ movie-info-service/src/test/java/com/systa/reactive/movie/info/repository/MovieInfoRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.systa.reactive.movie.info.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -27,7 +30,7 @@ class MovieInfoRepositoryTest {
 		List<MovieInfo> movies = Arrays.asList(
 				new MovieInfo(null, "Batman", "2021", Arrays.asList("abc", "xyz"), LocalDate.parse("2022-05-30")),
 				new MovieInfo(null, "Superman", "2022", Arrays.asList("abc", "xyz"), LocalDate.parse("2022-05-30")),
-				new MovieInfo(null, "Heman", "2023", Arrays.asList("abc", "xyz"), LocalDate.parse("2022-05-30"))
+				new MovieInfo("abc", "Heman", "2023", Arrays.asList("abc", "xyz"), LocalDate.parse("2022-05-30"))
 		);
 		
 		movieInfoRepository.saveAll(movies).blockLast();
@@ -42,9 +45,66 @@ class MovieInfoRepositoryTest {
 	@Test
 	void findAll() {
 		
-		var flux = movieInfoRepository.findAll();
+		var flux = movieInfoRepository.findAll().log();
 		StepVerifier.create(flux)
 			.expectNextCount(3)
+			.verifyComplete();
+		
+	}
+	
+	@Test
+	void findById() {
+		
+		var mono = movieInfoRepository.findById("abc").log();
+		StepVerifier.create(mono)
+			.assertNext(movieInfo -> {
+				assertEquals("Heman", movieInfo.getName());
+			})
+			.verifyComplete();
+		
+	}
+	
+	@Test
+	void save() {
+		
+		var mono = movieInfoRepository.save(
+				new MovieInfo(null, "Batman1", "2021", Arrays.asList("abc", "xyz"), 
+						LocalDate.parse("2022-05-30"))
+				).log();
+		
+		StepVerifier.create(mono)
+			.assertNext(movieInfo -> {
+				assertNotNull(movieInfo.getId());
+				assertEquals("Batman1", movieInfo.getName());
+			})
+			.verifyComplete();
+		
+	}
+	
+	@Test
+	void update() {
+		
+		var movieInfo = movieInfoRepository.findById("abc").block();
+		movieInfo.setYear("2012");
+		
+		var mono = movieInfoRepository.save(movieInfo).log();
+		
+		StepVerifier.create(mono)
+			.assertNext(movieInfo1 -> {
+				assertNotNull(movieInfo1.getId());
+				assertEquals("2012", movieInfo1.getYear());
+			})
+			.verifyComplete();
+		
+	}
+	
+	@Test
+	void delete() {
+		
+		movieInfoRepository.deleteById("abc").block();
+		var flux = movieInfoRepository.findAll().log();
+		StepVerifier.create(flux)
+			.expectNextCount(2)
 			.verifyComplete();
 		
 	}
