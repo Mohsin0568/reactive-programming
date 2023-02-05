4c980b29c54985e32f6e86cc8db1c8762b6d9a41
Mohsin0568 <mohsin68.murtuza@gmail.com>
2023-02-05 17:57:22 +0530

    changed router configurations to nest

--- movie-review-service/src/main/java/com/systa/reactive/movie/review/router/ReviewRouter.java
+++ movie-review-service/src/main/java/com/systa/reactive/movie/review/router/ReviewRouter.java
@@ -5,6 +5,7 @@ package com.systa.reactive.movie.review.router;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -22,10 +23,14 @@ public class ReviewRouter {
 	@Bean
 	public RouterFunction<ServerResponse> reviewRoutes(ReviewHandler handler){
 		return RouterFunctions.route()
+				.nest(RequestPredicates.path("/v1/review"), builder -> {
+					builder.GET("", request -> handler.getAllReviews());
+					builder.POST("", handler::saveReveiw);
+				})
 				.GET("/testEndpoint", (request -> ServerResponse.ok().bodyValue("test endpoint")))
-				.POST("/v1/review", handler::saveReveiw)
-				//.POST("/v1/review", request -> handler.saveReveiw(request))
-				.GET("/v1/review", request -> handler.getAllReviews())
+//				.POST("/v1/review", handler::saveReveiw)
+//				//.POST("/v1/review", request -> handler.saveReveiw(request))
+//				.GET("/v1/review", request -> handler.getAllReviews())
 				.build();
 	}
 }
